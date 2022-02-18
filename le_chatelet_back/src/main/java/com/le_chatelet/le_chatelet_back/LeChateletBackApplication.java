943d591f286d464785df55f45f704aa617cc63bf
Oguzhan KILIC <shinobi7755@gmail.com>
2022-02-18 09:47:39 +0100

    Add JavaSpring project for back

---
+++ le_chatelet_back/src/main/java/com/le_chatelet/le_chatelet_back/LeChateletBackApplication.java
@@ -0,0 +1,13 @@
+package com.le_chatelet.le_chatelet_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeChateletBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LeChateletBackApplication.class, args);
+    }
+
+}
---
+++ le_chatelet_back/src/test/java/com/le_chatelet/le_chatelet_back/LeChateletBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.le_chatelet.le_chatelet_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeChateletBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
