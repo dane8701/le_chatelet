34343258bb2aa8e3755c2807b6b8dd6af095c178
Oguzhan KILIC <shinobi7755@gmail.com>
2022-03-28 18:03:21 +0200

    Add mail sender

---
+++ le_chatelet_back/src/main/java/com/le_chatelet/le_chatelet_back/mail/MailConfiguration.java
@@ -0,0 +1,38 @@
+package com.le_chatelet.le_chatelet_back.mail;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.env.Environment;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+import java.util.Objects;
+import java.util.Properties;
+
+@Configuration
+public class MailConfiguration {
+
+    @Autowired
+    private Environment environment;
+
+    @Bean
+    public JavaMailSender getMailSender(){
+        JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+        mailSender.setHost(environment.getProperty("spring.mail.host"));
+        mailSender.setPort(Integer.parseInt(Objects.requireNonNull(environment.getProperty("spring.mail.port"))));
+        mailSender.setUsername(environment.getProperty("spring.mail.username"));
+        mailSender.setPassword(environment.getProperty("spring.mail.password"));
+
+        Properties javaMailProperties = new Properties();
+        javaMailProperties.put("mail.smtp.starttls.enable", "true");
+        javaMailProperties.put("mail.smtp.auth", "true");
+        javaMailProperties.put("mail.transport.protocol", "smtp");
+        javaMailProperties.put("mail.debug", "true");
+
+        mailSender.setJavaMailProperties(javaMailProperties);
+
+
+        return mailSender;
+    }
+}
